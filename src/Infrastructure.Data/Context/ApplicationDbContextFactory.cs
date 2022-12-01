9471f4e4055af07d065bac5a6e945e570c781307
Leonid Rubtsov <proflolstik98@mail.ru>
2022-12-01 14:22:16 +0300

    fix

--- src/Infrastructure.Data/Context/ApplicationDbContextFactory.cs
+++ src/Infrastructure.Data/Context/ApplicationDbContextFactory.cs
@@ -8,7 +8,7 @@ internal class ApplicationDbContextFactory : IDesignTimeDbContextFactory<Applica
     public ApplicationDbContext CreateDbContext(string[] args)
     {
         var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
-        optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=sample;Username=postgres;Password=postgres;");
+        optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=appDbName;Username=postgres;Password=postgres;");
 
         return new ApplicationDbContext(optionsBuilder.Options);
     }
