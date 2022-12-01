9d3cd9bbd66d099a6ac47a7c1443a23ed78cd5f2
Leonid Rubtsov <proflolstik98@mail.ru>
2022-12-01 13:59:33 +0300

    fix namespace

---
+++ src/Application/Handlers/TodoLists/TodoListCreate/TodoListCreateValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using LayeredTemplate.Application.Contracts.Requests;
+
+namespace LayeredTemplate.Application.Handlers.TodoLists.TodoListCreate;
+
+public class TodoListCreateValidator : AbstractValidator<TodoListCreateRequest>
+{
+    public TodoListCreateValidator()
+    {
+        this.RuleFor(x => x.Name)
+            .MaximumLength(25);
+    }
+}
