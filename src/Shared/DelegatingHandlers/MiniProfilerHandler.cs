f6c208660d4dd92f7a57fc7f37cf2c800dddd488
Leonid Rubtsov <proflolstik98@mail.ru>
2023-04-06 12:29:15 +0300

    fix

--- src/Shared/DelegatingHandlers/MiniProfilerHandler.cs
+++ src/Shared/DelegatingHandlers/MiniProfilerHandler.cs
@@ -8,7 +8,23 @@ public class MiniProfilerHandler : DelegatingHandler
         HttpRequestMessage request,
         CancellationToken cancellationToken)
     {
-        using var timing = MiniProfiler.Current?.CustomTiming("http", request.ToString());
+        if (MiniProfiler.Current == null)
+        {
+            return await base.SendAsync(request, cancellationToken);
+        }
+
+        string? requestContent = null;
+        if (request.Content != null)
+        {
+            requestContent = await request.Content.ReadAsStringAsync(cancellationToken);
+        }
+
+        if (!string.IsNullOrEmpty(requestContent))
+        {
+            requestContent = $"\nRequest Body:\n{requestContent}";
+        }
+
+        using var timing = MiniProfiler.Current?.CustomTiming("http", $"{request}{requestContent}");
 
         var response = await base.SendAsync(request, cancellationToken);
 
