f6a9827a1102dcc7745372f5b61d58a970f666be
Leonid Rubtsov <proflolstik98@mail.ru>
2023-04-06 12:15:06 +0300

    add miniprofiler for http clients

---
+++ src/Shared/DelegatingHandlers/MiniProfilerHandler.cs
@@ -0,0 +1,17 @@
+﻿using StackExchange.Profiling;
+
+namespace LayeredTemplate.Shared.DelegatingHandlers;
+
+public class MiniProfilerHandler : DelegatingHandler
+{
+    protected override async Task<HttpResponseMessage> SendAsync(
+        HttpRequestMessage request,
+        CancellationToken cancellationToken)
+    {
+        using var timing = MiniProfiler.Current?.CustomTiming("http", request.ToString());
+
+        var response = await base.SendAsync(request, cancellationToken);
+
+        return response;
+    }
+}
