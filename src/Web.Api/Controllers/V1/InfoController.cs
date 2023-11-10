c253e57e682d4472113896c8473e2b6a5eed7366
Leonid Rubtsov <proflolstik98@mail.ru>
2023-11-10 15:41:25 +0300

    add auto versionig

---
+++ src/Web.Api/Controllers/V1/InfoController.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LayeredTemplate.Web.Api.Controllers.V1;
+
+[ApiController]
+[Route("info")]
+public class InfoController : AppControllerBase
+{
+    [HttpGet("version")]
+    public ActionResult<string?> GetVersion()
+    {
+        return Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion;
+    }
+}
