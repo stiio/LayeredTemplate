c9f76cb7c4327d883782d3ac9087ed8d3a0b3141
Stio <proflolstik98@mail.ru>
2023-12-14 01:28:13 +0300

    f

--- src/Core/Web/OpenApiFilters/DefaultSchemaDescriptionFilter.cs
+++ src/Core/Web/OpenApiFilters/DefaultSchemaDescriptionFilter.cs
@@ -9,6 +9,7 @@ public class DefaultSchemaDescriptionFilter : ISchemaFilter
     public void Apply(OpenApiSchema schema, SchemaFilterContext context)
     {
         schema.Description ??= context.MemberInfo?.Name?.Humanize()
-                               ?? context.ParameterInfo?.Name?.Humanize();
+                               ?? context.ParameterInfo?.Name?.Humanize()
+                               ?? (context.Type.IsGenericType ? null : context.Type.Name.Humanize());
     }
 }
