103a001f2598288e176c95c23e43844cf826ec83
Stio <proflolstik98@mail.ru>
2024-10-19 17:58:07 +0300

    f

---
+++ src/Services/App/App.Web/Controllers/V1_dev/DebugController.cs
@@ -0,0 +1,19 @@
+﻿using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LayeredTemplate.App.Web.Controllers.V1_dev;
+
+[ApiController]
+[Route("debug")]
+public class DebugController : AppControllerBase
+{
+    public DebugController()
+    {
+    }
+
+    [HttpPost("test")]
+    public Task<IActionResult> Test()
+    {
+        return Task.FromResult<IActionResult>(this.Ok());
+    }
+}
