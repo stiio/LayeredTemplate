df222ad0bdbfacd5cd6e22684cb228c5ae2afb11
Stio <proflolstik98@mail.ru>
2026-05-19 14:18:25 +0300

    f

--- src/Services/App/App.Web/Shared/Options/AppSettings.cs
+++ src/Services/App/App.Web/Shared/Options/AppSettings.cs
@@ -3,23 +3,3 @@ namespace LayeredTemplate.App.Shared.Options;
 public sealed class AppSettings
 {
 }
-
-public sealed class SmtpSettings
-{
-    public string From { get; set; } = null!;
-
-    public string Host { get; set; } = null!;
-
-    public int Port { get; set; }
-
-    public string User { get; set; } = null!;
-
-    public string Password { get; set; } = null!;
-}
-
-public static class ConnectionStringKeys
-{
-    public const string WriteDb = "ConnectionStrings:AppWriteDbConnection";
-
-    public const string ReadDb = "ConnectionStrings:AppReadDbConnection";
-}
---
+++ src/Services/App/App.Web/Shared/Options/ConnectionStringKeys.cs
@@ -0,0 +1,8 @@
+﻿namespace LayeredTemplate.App.Shared.Options;
+
+public static class ConnectionStringKeys
+{
+    public const string WriteDb = "ConnectionStrings:AppWriteDbConnection";
+
+    public const string ReadDb = "ConnectionStrings:AppReadDbConnection";
+}
