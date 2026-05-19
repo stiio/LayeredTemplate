fb7f575f5472b578c7d4dfc8cb745270702e8c62
Stio <proflolstik98@mail.ru>
2026-05-19 14:19:17 +0300

    f

--- src/Services/App/App.Web/Shared/Pagination/QueryableExtensions.cs
+++ src/Services/App/App.Web/Shared/Pagination/QueryableExtensions.cs
@@ -5,32 +5,6 @@ namespace LayeredTemplate.App.Shared.Pagination;
 
 public static class QueryableExtensions
 {
-    public static Task<T?> FirstByIdOrDefault<T, TKey>(this IQueryable<T> query, TKey id, CancellationToken cancellationToken = default)
-        where T : class
-    {
-        var parameter = Expression.Parameter(typeof(T), "x");
-        var targetPropertyExpression = Expression.Property(parameter, "Id");
-        var sourceValueExpression = Expression.Constant(id);
-
-        var finalExpression = Expression.Equal(targetPropertyExpression, sourceValueExpression);
-        var lambda = Expression.Lambda<Func<T, bool>>(finalExpression, parameter);
-
-        return query.FirstOrDefaultAsync(lambda, cancellationToken);
-    }
-
-    public static Task<T> FirstById<T, TKey>(this IQueryable<T> query, TKey id, CancellationToken cancellationToken = default)
-        where T : class
-    {
-        var parameter = Expression.Parameter(typeof(T), "x");
-        var targetPropertyExpression = Expression.Property(parameter, "Id");
-        var sourceValueExpression = Expression.Constant(id);
-
-        var finalExpression = Expression.Equal(targetPropertyExpression, sourceValueExpression);
-        var lambda = Expression.Lambda<Func<T, bool>>(finalExpression, parameter);
-
-        return query.FirstAsync(lambda, cancellationToken);
-    }
-
     public static IQueryable<T> Page<T>(this IQueryable<T> query, PaginationRequest pagination) =>
         query.Skip((pagination.Page - 1) * pagination.Limit).Take(pagination.Limit);
 
