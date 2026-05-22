f7c27d31ebc8143182deff171dc286fbeba68d70
Stio <proflolstik98@mail.ru>
2026-05-22 22:27:10 +0300

    f

--- src/Services/App/App.Web/Shared/Errors/Exceptions/ForeignKeyViolationException.cs
+++ src/Services/App/App.Web/Shared/Errors/Exceptions/ForeignKeyViolationException.cs
@@ -2,7 +2,9 @@
 
 public sealed class ForeignKeyViolationException : DomainException
 {
-    public ForeignKeyViolationException(string message = "Cannot delete this item because it has dependent records. Remove all related items first.", Exception? innerException = null)
+    public ForeignKeyViolationException(
+        string message = "Cannot delete this item because it has dependent records. Remove all related items first.",
+        Exception? innerException = null)
         : base(message, innerException)
     {
     }
